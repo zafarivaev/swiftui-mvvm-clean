e18491eebecceaff3fae11547126ba70312b7929
zafarivaev <z.ivaev@mail.ru>
2021-06-28 20:45:47 +0500

    Initial Commit

---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Zafar on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
